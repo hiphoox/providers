6d338d1a7d268841afd6d9cb5e0155f3232d38fd
Norberto Ortigoza <norberto.ortigoza@raku.mx>
2012-03-12 17:06:37 -0600

    Changing footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  # Override build_footer method in ActiveAdmin::Views::Pages
+  require 'active_admin_views_pages_base.rb'
 end
---
+++ lib/active_admin_views_pages_base.rb
@@ -0,0 +1,12 @@
+class ActiveAdmin::Views::Pages::Base < Arbre::HTML::Document
+
+	private
+
+	# Renders the content for the footer
+	def build_footer
+		div :id => "footer" do
+			para "Copyrightttt &copy; #{Date.today.year.to_s} Powered by #{link_to('Raku', 'http://www.raku.com.mx')}".html_safe
+		end
+	end
+
+end
